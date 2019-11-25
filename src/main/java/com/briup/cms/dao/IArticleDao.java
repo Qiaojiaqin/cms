15ae9207d971625bbc1e3b667a0768b27eb29f90
qiaojq <qiaojia7808@163.com>
2019-11-25 10:17:33 +0800

    初始化项目

---
+++ src/main/java/com/briup/cms/dao/IArticleDao.java
@@ -0,0 +1,8 @@
+package com.briup.cms.dao;
+
+import com.briup.cms.bean.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IArticleDao extends JpaRepository<Article,Integer> {
+
+}
---
+++ src/main/java/com/briup/cms/dao/ICategoryDao.java
@@ -0,0 +1,8 @@
+package com.briup.cms.dao;
+
+import com.briup.cms.bean.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICategoryDao extends JpaRepository<Category,Integer> {
+
+}
